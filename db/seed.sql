551dc5934a74192de94eb73e64872744604d823a
Tawanda Moyo <moyotawanda@gmail.com>
2022-12-09 20:09:49 +0200

    Add default initial user for testing purposes to db

--- db/seed.sql
+++ db/seed.sql
@@ -3,6 +3,12 @@ CREATE DATABASE e_shop;
 
 \c e_shop;
 
+CREATE TYPE role_type AS ENUM
+(
+    'buyer',
+    'seller'
+);
+
 CREATE TYPE status AS ENUM
 (
     'pending',
@@ -12,32 +18,37 @@ CREATE TYPE status AS ENUM
 
 CREATE TABLE users
 (
-    id SERIAL,
-    username VARCHAR(100) NOT NULL
+    id              SERIAL,
+    username        VARCHAR(100)    NOT NULL,
+    email           VARCHAR(100)    NOT NULL,
+    role            role_type       NOT NULL
 );
 
 CREATE TABLE products
 (
-    product_id SERIAL,
-    product_title VARCHAR(100) NOT NULL,
-    product_desc TEXT,
-    seller_id INT NOT NULL,
-    price INT NOT NULL,
-    quantity INT,
-    image_url VARCHAR(100),
-    category VARCHAR(100)
+    product_id      SERIAL,
+    product_title   VARCHAR(100)    NOT NULL,
+    product_desc    TEXT,
+    seller_id       INT             NOT NULL,
+    price           INT             NOT NULL,
+    quantity        INT,
+    image_url       VARCHAR(100),
+    category        VARCHAR(100)
 );
 
 CREATE TABLE orders
 (
-    order_id SERIAL,
-    user_id INT NOT NULL,
-    order_status status NOT NULL
+    order_id        SERIAL,
+    user_id         INT NOT NULL,
+    order_status    status NOT NULL
 );
 
 CREATE TABLE order_products
 (
-    order_id INT NOT NULL,
-    product_id INT,
-    quantity INT
+    order_id        INT NOT NULL,
+    product_id      INT,
+    quantity        INT
 );
+
+INSERT INTO users (id, username, email, role)
+VALUES (1000001, 'admin', 'admin@eshop.com', 'seller');

0e223cefb04ed3829535d5391e972ef6c5c162cc
Tawanda Moyo <moyotawanda@gmail.com>
2023-01-12 13:20:18 +0200

    configure secrets

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const knex = require('./pgconfig');
-// const morgan = require('morgan');
+const secrets = require('./pgconfig');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const loginRouter = require('./routes/login');
@@ -14,14 +14,14 @@ const app = express();
 
 app.use(express.static('./backend/static'));
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser(secrets.cookieSecret));
 app.use(loginRouter);
 app.use(userRouter);
 app.use(orderRouter);
 app.use(productRouter);
 
 
-// The route below is a wildcard fallback route , may need to be more specific e.g 404 etc
+// The middleware below describes a catchall handler for any request that does not match any other route, 404??
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });

07c621895949e3c3eccb93bb0ad51abc05a985b0
Tawanda Moyo <moyotawanda@gmail.com>
2023-01-12 20:37:40 +0200

    configure google auth, retrieve user details

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,10 +1,15 @@
 const {Router} = require('express');
-
+const {OAuth2Client} = require('google-auth-library');
+const secrets = require('../../secrets.json');
+const GOOGLE_ID = secrets.googleId;
+const googleIdentity = new OAuth2Client(GOOGLE_ID);
 const router = Router();
 
 router.post('/login', async (req, res) => {
-    res.cookie('eshopLogin', req.body.credential);
-    console.log(req.cookies);
+    const loginToken = await googleIdentity.verifyIdToken({ idToken: req.body.credential, audience: GOOGLE_ID });
+    const payload = loginToken.getPayload();
+    const userEmail = payload.email;
+    res.cookie('eshopLogin', userEmail);
     res.sendStatus(200)
 
 });

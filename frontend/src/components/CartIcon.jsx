ffe546c6329467a78b0f7e2d9d9ae2492de1fba5
Tawanda Moyo <moyotawanda@gmail.com>
2023-02-08 18:50:17 +0200

    Add modal to cart

---
+++ frontend/src/components/CartIcon.jsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { useContext } from 'react';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { CartContext } from '../contexts/CartContextProvider';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import CartContents from '../views/CartContents';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function CustomizedBadges() {
+  const {cart} = useContext(CartContext);
+  console.log(cart);
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <IconButton aria-label="cart" onClick={handleOpen}>
+      <StyledBadge badgeContent={cart.length} color="secondary">
+        <ShoppingCartIcon />
+      </StyledBadge>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <CartContents cart={cart}/>
+        </Box>
+      </Modal>
+    </IconButton>
+  );
+}
